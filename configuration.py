3f2b6da8c0e0401eb2e969fcd738ef5f10c559b5
David Antonio Villalobos Lozano <tu_correo@example.com>
2026-03-30 11:07:07 -0600

    guardando cambios antes de pull

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-258c4ce0-5fd6-49c8-817e-83a7774c3d3f.containerhub.tripleten-services.com"
+BASE_URL = "https://cnt-dcbde619-272f-4b08-8a9a-50188aa01158.containerhub.tripleten-services.com?lng=es"
 
 CREATE_USER_PATH = "/api/v1/users"
 CREATE_KIT_PATH = "/api/v1/kits"
