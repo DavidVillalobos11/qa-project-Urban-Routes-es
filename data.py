4f12f41979eb7232adc66562fa324d640f7eef48
DavidVillalobos11 <dave_villalobos@hotmail.com>
2026-03-30 12:19:59 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-bd1d229b-25fd-4f51-8f3c-61cc196f7d6c.containerhub.tripleten-services.com"
+BASE_URL = "https://cnt-dcbde619-272f-4b08-8a9a-50188aa01158.containerhub.tripleten-services.com?lng=es"
 
 user_body = {
     "firstName": "David",
