3d7ce73327b4d9de2ac1439bbba258af2096197b
DavidVillalobos11 <dave_villalobos@hotmail.com>
2026-05-05 20:58:50 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-dcbde619-272f-4b08-8a9a-50188aa01158.containerhub.tripleten-services.com?lng=es"
+BASE_URL = "https://cnt-96b731b2-fc0b-4231-974f-c4ed34ea5c06.containerhub.tripleten-services.com?lng=es"
 
 CREATE_USER_PATH = "/api/v1/users"
 CREATE_KIT_PATH = "/api/v1/kits"
