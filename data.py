5bab0d044be673d5b9619fde7a153dc45b0ecdf1
DavidVillalobos11 <dave_villalobos@hotmail.com>
2026-05-05 21:01:36 -0600

    Update BASE_URL and add new variables

--- data.py
+++ data.py
@@ -1,11 +1,17 @@
-BASE_URL = "https://cnt-dcbde619-272f-4b08-8a9a-50188aa01158.containerhub.tripleten-services.com?lng=es"
+BASE_URL = "https://cnt-96b731b2-fc0b-4231-974f-c4ed34ea5c06.containerhub.tripleten-services.com?lng=es"
 
 user_body = {
     "firstName": "David",
     "phone": "+524491117913",
     "address": "Anaheim 212, Aguascalientes, Ags."
 }
-
+PHONE_NUMBER = "+521234567890"
 headers = {
     "Content-Type": "application/json"
 }
+PHONE = "1234567890"
+CODE = "1234"
+FROM = "East 2nd Street, 601"
+TO = "1300 1st St"
+CARD_NUMBER = "4111111111111111"
+CARD_CODE = "123"
